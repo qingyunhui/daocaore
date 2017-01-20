b7851f06439d1db8951061c4fc5744c20c9c005a
卿云辉 <qingyunhui@zuozh.com>
2017-01-20 17:11:37 +0800

    add file

---
+++ cn.com.yuzhushui.websocket/cn.com.yuzhushui.websocket.app/src/main/java/cn/com/yuzhushui/websocket/sys/web/action/WebSocketAction.java
@@ -0,0 +1,34 @@
+package cn.com.yuzhushui.websocket.sys.web.action;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+/***
+ ** @category 请用一句话来描述其用途...
+ ** @author qing.yunhui
+ ** @email: 280672161@qq.com
+ ** @createTime: 2017年1月20日下午3:01:51
+ **/
+@Controller
+@RequestMapping(WebSocketAction.ACTION_PATH)
+public class WebSocketAction {
+	
+	public static final String ACTION_PATH="/webSocketServer";
+	
+	Logger logger=LoggerFactory.getLogger(WebSocketAction.class);
+	
+	@RequestMapping(value="toIndex", method = {RequestMethod.POST, RequestMethod.GET})
+	public ModelAndView toIndex(){
+		ModelAndView mv=new ModelAndView();
+		
+		logger.info("toindex....");
+		
+		mv.setViewName("/index");
+		return mv;
+	}
+
+}
