ba9a2dac609283463b735087b9e3bac3be0fcadb
卿云辉 <qingyunhui@zuozh.com>
2017-06-14 16:43:58 +0800

    MongoDB demo

---
+++ mongodb-demo/mongodb-demo/src/main/java/com/junlenet/mongodb/demo/bo/Teach.java
@@ -0,0 +1,13 @@
+package com.junlenet.mongodb.demo.bo;
+
+import java.io.Serializable;
+
+/***
+ ** @category 请用一句话来描述其用途...
+ ** @author qing.yunhui
+ ** @email: 280672161@qq.com
+ ** @createTime: 2017年6月14日下午3:06:56
+ **/
+public class Teach implements Serializable{
+
+}
