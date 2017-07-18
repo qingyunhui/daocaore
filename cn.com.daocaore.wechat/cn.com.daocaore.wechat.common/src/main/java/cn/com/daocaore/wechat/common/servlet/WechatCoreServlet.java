a50dd42508abfb4156dc5405f116f48e541ff6f4
卿云辉 <qingyunhui@zuozh.com>
2017-07-18 18:08:07 +0800

    添加，微信用来验证开发者服务器

---
+++ cn.com.daocaore.wechat/cn.com.daocaore.wechat.common/src/main/java/cn/com/daocaore/wechat/common/servlet/WechatCoreServlet.java
@@ -0,0 +1,15 @@
+package cn.com.daocaore.wechat.common.servlet;
+
+import javax.servlet.http.HttpServlet;
+
+/***
+ ** @category 对微信提供的接口（微信用来验证开发者服务器）
+ ** @author qing.yunhui
+ ** @email: 280672161@qq.com
+ ** @createTime: 2017年7月18日下午6:00:48
+ **/
+public class WechatCoreServlet extends HttpServlet{
+
+	private static final long serialVersionUID = 1L;
+
+}
